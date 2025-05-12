117a09a91b5128e1a08ceb6355c9b5e377ca6734
levan kukhaleishvili <lkukhale@gmail.com>
2025-05-12 04:34:46 +0200

    added Project Card component, project cards first visual ready

--- Frontend/src/app/hand/hand.component.ts
+++ Frontend/src/app/hand/hand.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProjectCard } from '../services/project-cards.service';
+import { ProjectCardComponent } from '../project-card/project-card.component';
 
 @Component({
   selector: 'app-hand',
-  imports: [CommonModule],
+  imports: [CommonModule, ProjectCardComponent],
   templateUrl: './hand.component.html',
   styleUrl: './hand.component.css'
 })
